d9d3f7bee35f16cc593191378b3616aaded207e9
hsshin <skh1991515@naver.com>
2015-05-01 09:51:14 +0900

    hi

--- src/main/webapp/tour/local.js
+++ src/main/webapp/tour/local.js
@@ -4,16 +4,17 @@ myApp.controller('localController', function($scope) {
 	
 	$scope.click = function() {
 		alert("asdfdf");
-			
+		var url = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode?numOfRows=10&pageNo=1&MobileOS=AND&MobileApp=myxxx&_type=json&ServiceKey=";
+		var key = "sA7tgy37XyQzBU2fPZpZw%2BGKNlR0BPdgP2RhAvNrw4ls2so%2F%2BgeLDAT8AHJO6CacIlHvKIfubhwPjiDXpy%2B7%2Fw%3D%3D";
+		var openapi = url + key;
 			$.ajax({
+				
 				type : "GET",
-				url  : "local,
-				success : function(emp, status, xhr) {
-						alert(this.url +" : "+ JSON.stringify(emp));
-						var msg = "empno = " + emp.empno + "<br>" +
-								  "ename = " + emp.ename + "<br>" +
-								  "hiredate = " + new Date((emp.hiredate)).toLocaleDateString();
-						$('#get #result').html(msg);
+				url  : openapi,
+				dataType : "json",
+				success : function(local) {
+					alert("성공");
+					$('#output').val(JSON.stringify(local));
 				},	
 				error : function(xhr, status, error) {
 						alert(status);
